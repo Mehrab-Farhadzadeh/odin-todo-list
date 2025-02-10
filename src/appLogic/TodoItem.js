57641fe8a7a405253852b311ec65388d3905ef31
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2025-02-10 22:02:59 +0330

    Add toggleCheck

--- src/appLogic/TodoItem.js
+++ src/appLogic/TodoItem.js
@@ -1,3 +1,13 @@
-export default function (title, desc, dueDate, priority) {
-   return { title, desc, dueDate, priority, isChecked: false };
+export default function (
+   title,
+   desc,
+   dueDate = new Date(),
+   priority = "Medium"
+) {
+
+   function toggleCheck() {
+      this.isChecked = !this.isChecked;
+   }
+   
+   return { title, desc, dueDate, priority, isChecked: false, toggleCheck };
 }
