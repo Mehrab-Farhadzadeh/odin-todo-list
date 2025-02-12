644123027b6e95d35550396c39d36d34d8d12ec4
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2025-02-12 20:15:38 +0330

    Refactor: replace magic number

--- src/domLogic/domLogic.js
+++ src/domLogic/domLogic.js
@@ -4,8 +4,9 @@ import displayTodoList from "./displayTodoList";
 import addItemListener from "./addItemListener";
 
 export default function (AppLogic) {
+   const DEFAULT_PROJECT_IDX = 0;
    displayProjects(AppLogic.projects, AppLogic.getSelectedProjectId());
-   displayTodoList(AppLogic.projects[0].items);
+   displayTodoList(AppLogic.projects[DEFAULT_PROJECT_IDX].items);
    addProjectListener(AppLogic);
    addItemListener(AppLogic);
 }
