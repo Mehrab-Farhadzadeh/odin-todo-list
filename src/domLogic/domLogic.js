7310efd91bb62d66b433b9d8f149c97e7b8e00a9
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2025-02-12 21:22:37 +0330

    Add selectProjectListener

--- src/domLogic/domLogic.js
+++ src/domLogic/domLogic.js
@@ -2,11 +2,13 @@ import displayProjects from "./displayProjects";
 import addProjectListener from "./addProjectListener";
 import displayTodoList from "./displayTodoList";
 import addItemListener from "./addItemListener";
+import selectProjectListener from "./selectProjectListener";
 
 export default function (AppLogic) {
    const DEFAULT_PROJECT_IDX = 0;
-   displayProjects(AppLogic.projects, AppLogic.getSelectedProjectId());
+   displayProjects(AppLogic.projects, DEFAULT_PROJECT_IDX);
    displayTodoList(AppLogic.projects[DEFAULT_PROJECT_IDX].items);
    addProjectListener(AppLogic);
    addItemListener(AppLogic);
+   selectProjectListener(AppLogic);
 }
---
+++ src/domLogic/selectProjectListener.js
@@ -0,0 +1,13 @@
+import displayProjects from "./displayProjects";
+import displayTodoList from "./displayTodoList";
+
+export default function (AppLogic) {
+   const ul = document.querySelector("ul.projects");
+
+   ul.addEventListener("click", (e) => {
+      const selectedProjectId = +e.target.dataset.id;
+      AppLogic.setSelectedProjectId(selectedProjectId);
+      displayProjects(AppLogic.projects, selectedProjectId);
+      displayTodoList(AppLogic.projects[selectedProjectId].items);
+   });
+}
