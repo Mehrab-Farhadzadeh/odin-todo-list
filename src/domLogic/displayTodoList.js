9ae065e5fe119ce088080785eb321e41b310e009
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2025-02-15 13:12:03 +0330

    Add dueDate and priority, refactor

--- src/domLogic/displayTodoList.js
+++ src/domLogic/displayTodoList.js
@@ -1,3 +1,28 @@
+import { format } from "date-fns";
+
+function createTodoItem(item) {
+   const newLi = document.createElement("li");
+   newLi.classList.add("todo-item");
+   newLi.setAttribute("data-id", item.id);
+
+   // title
+   const titleDiv = document.createElement("div");
+   titleDiv.textContent = item.title;
+   titleDiv.classList.add("title");
+   newLi.appendChild(titleDiv);
+
+   // dueDate
+   const dueDateDiv = document.createElement("div");
+   dueDateDiv.textContent = format(item.dueDate, "dd-MMM-yyyy");
+   dueDateDiv.classList.add("due-date");
+   newLi.appendChild(dueDateDiv);
+
+   // proiority
+   newLi.classList.add(item.priority.toLowerCase());
+
+   return newLi;
+}
+
 export default function (AppLogic) {
    const selectedProject = AppLogic.getSelectedProject();
    const ul = document.querySelector("ul.todo-list");
@@ -14,11 +39,7 @@ export default function (AppLogic) {
    todoListTitle.textContent = selectedProject.title;
    // Display items
    for (const todoItem of selectedProject.items) {
-      const newLi = document.createElement("li");
-      newLi.classList.add("todo-item");
-      newLi.setAttribute("data-id", todoItem.id);
-      newLi.textContent = todoItem.title;
-      ul.append(newLi);
+      ul.append(createTodoItem(todoItem));
    }
    // Add seleceted class
    const selectedItem = AppLogic.getSelectedProject().getSelectedItem();
